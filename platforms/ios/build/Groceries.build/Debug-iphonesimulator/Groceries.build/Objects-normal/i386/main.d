9447376e1f1fe58840eef7d732db13beb0d24a1f
chance <chancemagno@gmail.com>
2017-08-12 13:05:13 -0700

    create basic app exercise layout

---
+++ platforms/ios/build/Groceries.build/Debug-iphonesimulator/Groceries.build/Objects-normal/i386/main.d
@@ -0,0 +1,10 @@
+dependencies: \
+  /Users/chance/Desktop/NativeScript\ Apps/Groceries/platforms/ios/Groceries/Groceries-Prefix.pch \
+  /Users/chance/Desktop/NativeScript\ Apps/Groceries/platforms/ios/internal/main.m \
+  /Users/chance/Desktop/NativeScript\ Apps/Groceries/platforms/ios/internal/NativeScript.framework/Headers/NativeScript.h \
+  /Users/chance/Desktop/NativeScript\ Apps/Groceries/platforms/ios/internal/NativeScript.framework/Headers/TNSRuntime+Diagnostics.h \
+  /Users/chance/Desktop/NativeScript\ Apps/Groceries/platforms/ios/internal/NativeScript.framework/Headers/TNSRuntime.h \
+  /Users/chance/Desktop/NativeScript\ Apps/Groceries/platforms/ios/internal/NativeScript.framework/Headers/TNSRuntime+Inspector.h \
+  /Users/chance/Desktop/NativeScript\ Apps/Groceries/platforms/ios/internal/TNSExceptionHandler.h \
+  /Users/chance/Desktop/NativeScript\ Apps/Groceries/platforms/ios/internal/TNSDebugging.h \
+  /Users/chance/Desktop/NativeScript\ Apps/Groceries/platforms/ios/internal/TKLiveSync/include/TKLiveSync.h
